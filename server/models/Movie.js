0e16441916985e4ae77315741eadc5de4901de38
Aida-Gheorghe <aida.andreea.gheorghe@digitalexplorer.ro>
2021-06-09 19:24:50 +0300

    Final

--- server/models/Movie.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-  movieId: String, // TMDB movieId
-  name: String,
-  year: Number,
-});
-
-module.exports = mongoose.model('Movie', schema);
--- server/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true },
-  firstName: String,
-  lastName: String,
-});
-
-module.exports = mongoose.model('User', schema);
